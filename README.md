56d8a5bfcaaeedfea3bc4d10cfd2ed6e028cf9e3
himesh-m <himesh.mistry@mindbowser.com>
2024-02-27 19:24:01 +0530

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# wifi_iot_example
+
+Demonstrates how to use the wifi_iot plugin.
+
+## Getting Started
+
+For help getting started with Flutter, view online
+[documentation](https://flutter.io/).
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/morning_star/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/morning_star/Downloads/WiFiFlutter-master/packages/wifi_iot/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/morning_star/Downloads/WiFiFlutter-master/packages/wifi_iot/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNDBlOTc2YmVkZmY1N2U2OWUxYjNkODlhN2MyYTNjNjE3YTAzZGFkLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/morning_star/Downloads/WiFiFlutter-master/packages/wifi_iot/example/.dart_tool/package_config.json"
